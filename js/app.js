981f7987fd433ef25f6444236c01ee5a8bef871e
zocha_sz <zofia.szczudlik@gmail.com>
2017-05-25 19:51:17 +0200

    roll works

--- js/app.js
+++ js/app.js
@@ -5,34 +5,70 @@ window.onload = function () {
 }
 
 
-document.getElementById("roll").addEventListener("click", rollFirstTurn);
+document.getElementById("roll").addEventListener("click", player1RollFirstTurn);
 
 var array1 = [];
-function rollFirstTurn(){
+function player1RollFirstTurn(){
   for (i=1; i<6; i++){
     var number = game.rollOneDice();
     document.getElementById("dice"+i).innerHTML = number;
     array1.push(number);
+    document.getElementById("dice"+i).disabled = false;
+    document.getElementById("dice"+i).addEventListener("click", player1SaveForNextRoll);
   }
+  document.getElementById("roll").removeEventListener("click", player1RollFirstTurn);
 };
 
-var player1Dice1 = {
-  value: 0,
-  status: true
+function player1SaveForNextRoll(){
+    var target = event.target;
+    for (i=1; i<6; i++){
+      if (target.id ==="dice"+i){document.getElementById("dice"+i).disabled = true;}
+    }
+
+    document.getElementById("roll").addEventListener("click", player1RollSecondTurn);
 };
-var player1Dice2 = {
-  value: 0,
-  status: true
+
+function player1RollSecondTurn(){
+  for (i=1; i<6; i++){
+    if (document.getElementById("dice"+i).disabled === false){
+      var number = game.rollOneDice();
+      document.getElementById("dice"+i).innerHTML = number;
+      array1[i-1] = number;
+    }
+    document.getElementById("roll").removeEventListener("click", player1RollSecondTurn);
+    document.getElementById("roll").addEventListener("click", player2RollFirstTurn);
+  }
+}
+
+
+var array2 = [];
+function player2RollFirstTurn(){
+  for (i=6; i<11; i++){
+    var number = game.rollOneDice();
+    document.getElementById("dice"+i).innerHTML = number;
+    array2.push(number);
+    document.getElementById("dice"+i).disabled = false;
+    document.getElementById("dice"+i).addEventListener("click", player2SaveForNextRoll);
+  }
+  document.getElementById("roll").removeEventListener("click", player2RollFirstTurn);
 };
-var player1Dice3 = {
-  value: 0,
-  status: true
+
+function player2SaveForNextRoll(){
+    var target = event.target;
+    for (i=6; i<11; i++){
+      if (target.id ==="dice"+i){document.getElementById("dice"+i).disabled = true;}
+    }
+
+    document.getElementById("roll").addEventListener("click", player2RollSecondTurn);
 };
-var player1Dice4 = {
-  value: 0,
-  status: true
-};
-var player1Dice5 = {
-  value: 0,
-  status: true
-};
+
+function player2RollSecondTurn(){
+  for (i=6; i<11; i++){
+    if (document.getElementById("dice"+i).disabled === false){
+      var number = game.rollOneDice();
+      document.getElementById("dice"+i).innerHTML = number;
+      array2[i-6] = number;
+    }
+    document.getElementById("roll").removeEventListener("click", player2RollSecondTurn);
+  }
+}

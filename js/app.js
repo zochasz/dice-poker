62c9dce60e69403a4c12e76c714f0c6026d7b75e
zocha_sz <zofia.szczudlik@gmail.com>
2017-06-10 14:11:02 +0200

    game over improvements

--- js/app.js
+++ js/app.js
@@ -66,6 +66,13 @@ window.onload = function () {
           game.round+=1;
           game.turn=1;
           game.currentPlayer = 1;
+          if (player1.credits <=0 || player2.credits<=0){
+            game.round = 1;
+            game.player1.credits = 6;
+            document.getElementById("js-credits1").innerHTML = 6+" ";
+            game.player2.credits = 6;
+            document.getElementById("js-credits2").innerHTML = 6+" ";
+          }
         }
       }
       document.getElementById("next").disabled = true;
